1b71834278396903daa1f331a86e9ce4b965f598
ahvino <seliksamai@gmail.com>
2024-04-22 19:37:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Satsuma;
+using Unchase.Satsuma.Core;
+
+class Graph
+{
+    void CreateGraph()
+    {
+        CustomGraph graph = new CustomGraph();
+
+        Satsuma.Node a = graph.AddNode();
+        Satsuma.Node b = graph.AddNode();
+        Satsuma.Node c = graph.AddNode();    
+
+        Satsuma.Arc ab = graph.AddArc(a, b, Directedness.Undirected);
+        Satsuma.Arc bc = graph.AddArc(b, c, Directedness.Undirected);
+
+    }
+
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
+        Graph graph = new Graph();
+        graph.CreateGraph();
+    }
+}
